2ca9b3da8422e3081f3852c44fc7aff0bd6f297d
anondo <gazimohammadanondo@gmail.com>
2025-10-10 16:05:28 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 dotenv.config();
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
